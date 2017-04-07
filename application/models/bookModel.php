5674dc845ebc9753da4b5b830f65ac0a54dd7698
davendra <davendra@intellectstorm.com>
2017-04-07 13:26:42 -0400

    added book model

---
+++ application/models/bookModel.php
@@ -0,0 +1,44 @@
+<?php
+
+class bookModel extends CI_Model{
+
+  var $subject; 
+  var $title;
+  var $author;
+  var $coAuthor;
+  var $publisher;
+  var $edition;
+  var $page;
+  var $isbn;
+  var $noOfBooks; 
+  var $active;
+
+  function __construct(){
+    parent::__construct();
+  }
+  
+  function addBook(){
+    
+  }
+  
+  function viewBook(){
+    
+  }
+  
+  function updateBook(){
+    
+  }
+  
+  function deleteBook(){
+    
+  }
+  
+  function borrowBook(){
+    
+  }
+  
+  function returnBook(){
+    
+  }
+}
+?>
