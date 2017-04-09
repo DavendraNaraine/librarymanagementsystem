b8c3a28980ae462cf77549457b3bececd24f88e9
davendra <davendra@intellectstorm.com>
2017-04-08 22:27:07 -0400

    added controllers

---
+++ application/models/Member_model.php
@@ -0,0 +1,27 @@
+<?php
+
+class Member_model extends CI_Model{
+  
+  var $usi;
+  
+  function __construct(){
+    parent::__construct();
+  }
+  
+  function addMember(){
+    
+  }
+  
+  function viewMember(){
+    
+  }
+  
+  function updateMember(){
+    
+  }
+  
+  function deleteMember(){
+    
+  }
+}
+?>
