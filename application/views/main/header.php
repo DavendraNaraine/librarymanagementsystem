3926df46dde51429d53b1016ddb5f4b574f652f3
amrita <amrita@intellectstorm.com>
2017-04-11 01:43:26 -0400

    Change image source

--- application/views/main/header.php
+++ application/views/main/header.php
@@ -21,7 +21,7 @@
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#"><img src="../images/ug-logo.png" alt="ug-logo" style="max-width:60%; margin-top: -7px; margin-bottom: -7px;"></a>
+            <a class="navbar-brand" href="#"><img src="<?=$this->config->base_url()?>res/images/ug-logo.png" alt="ug-logo" style="max-width:60%; margin-top: -7px; margin-bottom: -7px;"></a>
           </div>
 
           <!-- Collect the nav links, forms, and other content for toggling -->
