b5f7bc798ee1ede5f5ebf7924cd65b00a869f48e
amrita <amrita@intellectstorm.com>
2017-04-11 01:39:52 -0400

    View books design added

---
+++ application/views/main/view-books.php
@@ -0,0 +1,38 @@
+<div class="container">
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Subject</th>
+                <th>Title</th>
+                <th>Author</th>
+                <th>Publisher</th>
+                <th>Edition</th>
+                <th> </th>
+            </tr>
+        </thead>
+        <tbody>
+            <!--Dynamic content will have to go here-->
+            <tr>
+                <th scope="row">1</th>
+                <td>Database</td>
+                <td>Database Design for Noobs</td>
+                <td>Sir G</td>
+                <td>University of Ghana</td>
+                <td>7th</td>
+                <td>
+                    <button type="button" class="btn btn-success" aria-label="Left Align">
+          Edit <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+        </button>
+                    <button type="button" class="btn btn-success" aria-label="Left Align">
+          Delete <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+        </button>
+                    <button type="button" class="btn btn-success" aria-label="Left Align">
+          Borrow <span class="glyphicon glyphicon-save" aria-hidden="true"></span>
+        </button>
+                </td>
+            </tr>
+
+        </tbody>
+    </table>
+</div>
