4fe02872e4adbe14137f36fa4d5f4aba4991514e
davendra <davendra@intellectstorm.com>
2017-04-20 21:48:28 -0400

    completed update title functionality

--- application/models/Titles_model.php
+++ application/models/Titles_model.php
@@ -34,7 +34,28 @@ class Titles_model extends CI_Model{
   }
   
   function updateTitle($title_id){
+    $update = array (
+      'title_name' => $this->input->post('title'),
+      'title_author' => $this->input->post('author'),
+      'title_coauthor' => $this->input->post('coAuthor'),
+      'title_edition' => $this->input->post('edition'),
+      'title_publisher' => $this->input->post('publisher'),
+      'title_isbn' => $this->input->post('isbn')
+    );
     
+    $this->db->select('*');
+    $this->db->from('titles');
+    $this->db->where('title_id', $title_id);
+    $query = $this->db->get();
+    
+    if($query->num_rows() > 0){
+      $this->db->where('title_id', $title_id);
+      $this->db->update('titles', $update);
+      return $title_id;
+    }
+    else{
+      return 'Title cannot be updated';
+    }
   }
   
   function addTitle(){
@@ -44,8 +65,7 @@ class Titles_model extends CI_Model{
       'title_coauthor' => $this->input->post('coAuthor'),
       'title_edition' => $this->input->post('edition'),
       'title_publisher' => $this->input->post('publisher'),
-      'title_isbn' => $this->input->post('isbn'),
-      'title_copies' => $this->input->post('copies') 
+      'title_isbn' => $this->input->post('isbn')
     );
     
     $this->db->select('title_isbn');
